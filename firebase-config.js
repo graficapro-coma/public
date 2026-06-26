890f62eacd0e0bcb2a8ba7fc876362402183a752
graficapro-coma <info@agflorida.com.ar>
2026-06-26 09:28:26 -0300

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.__FBCFG = {
+  apiKey: "…",            // ← pegá acá los valores reales que copiaste
+  authDomain: "graficapro-coma.firebaseapp.com",
+  projectId: "graficapro-coma",
+  storageBucket: "graficapro-coma.firebasestorage.app",
+  messagingSenderId: "…",
+  appId: "…"
+};
