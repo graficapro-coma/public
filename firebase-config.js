dc3bbc884c5b0f08b75dae7e8027b5443c7cd4cb
graficapro-coma <info@agflorida.com.ar>
2026-06-26 09:43:26 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.__FBCFG = {
-  apiKey: "…",            // ← pegá acá los valores reales que copiaste
+  apiKey: "AIzaSyBknhCg3HU7x1qa1hEgpSpt8yeA3QSBf9g",            
   authDomain: "graficapro-coma.firebaseapp.com",
   projectId: "graficapro-coma",
   storageBucket: "graficapro-coma.firebasestorage.app",
-  messagingSenderId: "…",
-  appId: "…"
+  messagingSenderId: "439415860272",
+  appId: "1:439415860272:web:f3b8860f6e5457111291f5"
 };
